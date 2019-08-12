ee5a83c93b0f517b9d797f6dedb5baa996359296
William Comartin <williamcomartin@gmail.com>
2019-08-12 08:13:42 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
     name='KiaUvo',
     packages=['KiaUvo'],
-    version='0.2',
+    version='0.3',
     license='MIT',
     description='API Wrapper for the Kia Uvo service',
     author='William Comartin',
     author_email='williamcomartin@gmail.com',
     url='https://github.com/wcomartin/kiauvo',
-    download_url='https://github.com/wcomartin/kiauvo/archive/v_02.tar.gz',
+    download_url='https://github.com/wcomartin/kiauvo/archive/v_03.tar.gz',
     keywords=['Kia', 'Uvo', 'Api'],
     install_requires=[
         'requests',
